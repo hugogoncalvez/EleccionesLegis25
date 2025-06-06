5f3ae0daf0305759eadfe200c40f4bc18938b899
hugogoncalvez <hugogoncalvez@gmail.com>
2025-06-06 08:01:13 -0300

    se cambia BD
    se cambia a clever cloud

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const PORT = process.env.PORT || 8000
 
-export const DB_HOST = process.env.DB_HOST || 'sql10.freesqldatabase.com'
-export const DB_USER = process.env.DB_USER || 'sql10782145'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'm2RZIAkNIT'
+export const DB_HOST = process.env.DB_HOST || 'b802lrxvalwgrdrlj5eh-mysql.services.clever-cloud.com'
+export const DB_USER = process.env.DB_USER || 'udakscmgby6aaehz'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'KOS6NViwEphG3iqprWdn'
 //export const DB_PASSWORD = process.env.DB_PASSWORD || 'ciara'
-export const DB_NAME = process.env.DB_NAME || 'sql10782145'
+export const DB_NAME = process.env.DB_NAME || 'b802lrxvalwgrdrlj5eh'
 export const DB_PORT = process.env.DB_PORT || 3306
